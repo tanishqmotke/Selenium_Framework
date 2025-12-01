61186f204704ccc00fc1b872aaf103749ca8bb6c
tanishq <tmotke98@gmail.com>
2025-12-01 13:33:40 +0000

    Selenium Framework Updated

---
+++ src/test/java/framework/SeleniumFramework/StandAloneTest.java
@@ -0,0 +1,51 @@
+package framework.SeleniumFramework;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+
+public class StandAloneTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		String ProductName = "ZARA COAT 3";
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://rahulshettyacademy.com/client/#/auth/login");
+
+		driver.findElement(By.id("userEmail")).sendKeys("tanishqmotke110@gmail.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Pass@123");
+		driver.findElement(By.id("login")).click();
+		WebDriverWait Wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+		Wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+
+		WebElement prod = products.stream()
+				.filter(product -> product.findElement(By.cssSelector("b")).getText().equals(ProductName)).findFirst()
+				.orElse(null);
+
+		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
+		Wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		Wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		Thread.sleep(5000);
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+		Boolean match = cartProducts.stream()
+				.anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(ProductName));
+		Assert.assertTrue(match);
+		driver.close();
+		System.out.println("Executed");
+	}
+
+}
